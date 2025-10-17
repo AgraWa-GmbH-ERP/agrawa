36e409d87184472893b7148764c1a6e992628c53
Furqan Asghar <furqan.79000@gmail.com>
2025-10-17 11:06:58 +0500

    feat: applying show_qualification_card feature on quotation, sales order, sales invoice

--- agrawa/hooks.py
+++ agrawa/hooks.py
@@ -13,8 +13,10 @@ doctype_list_js = {
 app_include_js = "agrawa.bundle.js"
 
 doctype_js = {
+    "Quotation": "public/js/quotation.js",
     "Customer": "public/js/customer.js",
     "Sales Order": "public/js/sales_order.js",
+    "Sales Invoice": "public/js/sales_invoice.js",
     "Purchase Invoice": "public/js/custom_purchase_invoice.js"
 }
 
