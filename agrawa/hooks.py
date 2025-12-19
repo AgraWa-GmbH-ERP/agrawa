cb2ce57d8c4c1410db9321c9d7baecbd29b2f9a7
Furqan Asghar <furqan.79000@gmail.com>
2025-12-19 18:35:29 +0500

    feat: update sales invoice event handlers for collective order allocation management

--- agrawa/hooks.py
+++ agrawa/hooks.py
@@ -36,10 +36,9 @@ doctype_js = {
 doc_events = {
     "Sales Invoice": {
         "on_submit": [
-            "agrawa.overrides.custom_sales_invoice.update_purchase_invoice_status_on_billing",
-            "agrawa.overrides.custom_sales_invoice.set_sales_invoice_on_collective_sales_order"
+            "agrawa.overrides.custom_sales_invoice.update_purchase_invoice_status_on_billing"
         ],
-        "on_cancel": "agrawa.overrides.custom_sales_invoice.unset_sales_invoice_on_collective_sales_order"
+        "on_cancel": "agrawa.overrides.custom_sales_invoice.unset_sales_invoice_on_sales_order_item_allocation"
     },
     "Sales Order": {
         "before_save": "agrawa.overrides.custom_sales_order.set_dropshipping_data"
