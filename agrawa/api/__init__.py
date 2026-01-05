a5d8fa35b40584ceb41f02236b8cb698f83eb39b
Furqan Asghar <furqan.79000@gmail.com>
2026-01-05 19:40:07 +0500

    feat: refactor sales invoice creation process in collective order allocation to make it similar to manual sales invoice creation

--- agrawa/api/__init__.py
+++ agrawa/api/__init__.py
@@ -1,5 +1,9 @@
 import json
 import frappe
+from frappe.model.mapper import get_mapped_doc
+from frappe.contacts.doctype.address.address import get_company_address
+from frappe.model.utils import get_fetch_values
+from erpnext.accounts.party import get_party_account
 from agrawa.overrides.custom_sales_order import validate_allocation_quantities
 
 
@@ -41,52 +45,112 @@ def add_alocations_and_create_invoice(sales_order, allocations):
 		allocations = json.loads(allocations)
 		allocations = [frappe._dict(allocation) for allocation in allocations]
 
-
 	for allocation in allocations:
 		del allocation["name"]
 	
 	so_doc = frappe.get_doc('Sales Order', sales_order)	
 
 	for allocation in allocations:
-		si = frappe.new_doc("Sales Invoice")
-		si.customer = allocation["customer"]
-		si.posting_date = frappe.utils.today()
-		si.set_posting_time = 0
+		# Create Sales Invoice using get_mapped_doc
+		si = create_sales_invoice_from_allocation(so_doc, allocation)
 		
-		si.company = so_doc.company
-		si.currency = so_doc.currency
-		si.conversion_rate = so_doc.conversion_rate
-		si.selling_price_list = so_doc.selling_price_list
-		si.price_list_currency = so_doc.price_list_currency
-		si.plc_conversion_rate = so_doc.plc_conversion_rate
-		si.ignore_pricing_rule = so_doc.ignore_pricing_rule
-
-		if allocation["customer"] == so_doc.customer:
-			si.customer_address = so_doc.customer_address
-			si.contact_person = so_doc.contact_person
-			si.shipping_address_name = so_doc.shipping_address_name
-		si.append("items", {
-			"item_code": allocation.item_code,
-			"item_name": allocation.item_name,
-			"description": allocation.description,
-			"qty": allocation.allocated_qty,
-			"uom": allocation.uom,
-			"stock_uom": allocation.stock_uom,
-			"conversion_factor": allocation.conversion_factor,
-			"rate": allocation.rate,
-			"amount": allocation.amount,
-			"warehouse": allocation.warehouse
-		})
-
-		si.save()
-		si.submit()
-
 		allocation["sales_invoice"] = si.name
-		
 		so_doc.append('custom_item_allocation', allocation)
 
 	validate_allocation_quantities(so_doc)
-
 	so_doc.save()
 
 	return {"sales_order": sales_order, "allocations_added": len(allocations)}
+
+
+def create_sales_invoice_from_allocation(so_doc, allocation):
+	"""Create Sales Invoice from Sales Order using get_mapped_doc"""
+	
+	def postprocess(source, target):
+		set_missing_values(source, target, allocation)
+		
+	def set_missing_values(source, target, allocation):
+		# Override customer from allocation
+		target.customer = allocation["customer"]
+		target.posting_date = frappe.utils.today()
+		target.set_posting_time = 0
+		
+		target.flags.ignore_permissions = True
+		target.run_method("set_missing_values")
+		target.run_method("set_po_nos")
+		target.run_method("calculate_taxes_and_totals")
+		target.run_method("set_use_serial_batch_fields")
+
+		# # Set address details based on customer
+		# if allocation["customer"] == source.customer:
+		# 	if source.customer_address:
+		# 		target.customer_address = source.customer_address
+		# 	if source.contact_person:
+		# 		target.contact_person = source.contact_person
+		# 	if source.shipping_address_name:
+		# 		target.shipping_address_name = source.shipping_address_name
+		
+		# if source.company_address:
+		# 	target.update({"company_address": source.company_address})
+		# else:
+		# 	target.update(get_company_address(target.company))
+
+		# if target.company_address:
+		# 	target.update(get_fetch_values("Sales Invoice", "company_address", target.company_address))
+
+		target.debit_to = get_party_account("Customer", target.customer, source.company)
+
+	def update_item(source, target, source_parent):
+		# Set item details from allocation
+		target.qty = allocation.allocated_qty
+		target.rate = allocation.rate
+		target.amount = allocation.amount
+		target.warehouse = allocation.warehouse
+		
+		if source_parent.project:
+			target.cost_center = frappe.db.get_value("Project", source_parent.project, "cost_center")
+
+	def should_map_item(source_item):
+		# Only map the specific item from the allocation
+		return source_item.item_code == allocation.item_code
+
+	si = get_mapped_doc(
+		"Sales Order",
+		so_doc.name,
+		{
+			"Sales Order": {
+				"doctype": "Sales Invoice",
+				"field_map": {
+					"party_account_currency": "party_account_currency",
+					"payment_terms_template": "payment_terms_template",
+				},
+				"field_no_map": ["payment_terms_template", "customer_address", "shipping_address_name"],
+				"validation": {"docstatus": ["=", 1]},
+			},
+			"Sales Order Item": {
+				"doctype": "Sales Invoice Item",
+				# "field_map": {
+				# 	"name": "so_detail",
+				# 	"parent": "sales_order",
+				# },
+				"postprocess": update_item,
+				"condition": lambda doc: should_map_item(doc),
+			},
+			"Sales Taxes and Charges": {
+				"doctype": "Sales Taxes and Charges",
+				"reset_value": True,
+			},
+			"Sales Team": {
+				"doctype": "Sales Team", 
+				"add_if_empty": True
+			},
+		},
+		None,
+		postprocess,
+		ignore_permissions=True,
+	)
+	
+	si.save()
+	si.submit()
+	
+	return si
