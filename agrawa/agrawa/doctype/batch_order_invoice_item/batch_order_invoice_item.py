4f6ec868a99fa9929a693c20d418d1eac30d2fed
Furqan Asghar <furqan.79000@gmail.com>
2026-02-18 15:45:24 +0500

    feat: add Batch Order Invoice Item doctype and related fields for invoicing info in collective sales order

---
+++ agrawa/agrawa/doctype/batch_order_invoice_item/batch_order_invoice_item.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2026, Phamos GmbH and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+
+class BatchOrderInvoiceItem(Document):
+	pass
