45e8bb9872ccbe01cc703c730026d99ae884fe0d
Kalsoom Akhtar <kalsoom.akhtar@phamos.eu>
2026-03-03 16:38:56 +0500

    Additional discount not applyed on excluded items in agrawa setting

---
+++ agrawa/agrawa/doctype/agrawa_settings/agrawa_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Phamos GmbH and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Agrawa Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
